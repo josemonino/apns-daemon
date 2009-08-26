d0f55ed0db852bb66bb834540356a27b750c2774
spanyam <spanyam@localhost>
2009-08-26 13:01:19 +1000

    adding sample config

---
+++ config.py
@@ -0,0 +1,16 @@
+
+data = {
+    {
+        'appname': "App1",
+        'bundle_id': "App1 BundleID",
+        'certificate_file':    "path_to_certificate_file_1",
+        'privatekey_file':     "path_to_privatekey_file_1"
+    },
+    {
+        'appname': "App2",
+        'bundle_id': "App2 BundleID",
+        'certificate_file':    "path_to_certificate_file_2",
+        'privatekey_file':     "path_to_privatekey_file_2"
+    },
+}
+
