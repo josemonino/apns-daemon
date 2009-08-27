093eb32c598226425d2ebb7a536445f95980c5f4
spanyam <spanyam@localhost>
2009-08-27 17:57:16 +1000

    formatting

--- constants.py
+++ constants.py
@@ -16,12 +16,13 @@
 #
 ###############################################################################
 
-MAX_PAYLOAD_LENGTH = 256
+MAX_PAYLOAD_LENGTH          = 256
+DEVICE_TOKEN_LENGTH         = 32
 
-DEFAULT_APNS_PROD_HOST  = "gateway.push.apple.com"
-DEFAULT_APNS_DEV_HOST   = "gateway.sandbox.push.apple.com"
-DEFAULT_APNS_PROD_PORT  = 2195
-DEFAULT_APNS_DEV_PORT   = 2195
+DEFAULT_APNS_PROD_HOST      = "gateway.push.apple.com"
+DEFAULT_APNS_DEV_HOST       = "gateway.sandbox.push.apple.com"
+DEFAULT_APNS_PROD_PORT      = 2195
+DEFAULT_APNS_DEV_PORT       = 2195
 
 DEFAULT_FEEDBACK_PROD_HOST  = "feedback.push.apple.com"
 DEFAULT_FEEDBACK_DEV_HOST   = "feedback.sandbox.push.apple.com"
