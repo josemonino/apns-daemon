a1daa8c51bd3838c053c1fc8ca35e1e1c52dc019
spanyam <spanyam@localhost>
2009-08-27 14:52:19 +1000

    Added APL license.

--- sample_config.py
+++ sample_config.py
@@ -1,16 +1,44 @@
+###############################################################################
+#
+# Copyright 2009, Sri Panyam
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
 
-data = {
-    {
-        'appname': "App1",
-        'bundle_id': "App1 BundleID",
-        'certificate_file':    "path_to_certificate_file_1",
-        'privatekey_file':     "path_to_privatekey_file_1"
+{
+    'clients': {
+        'line': {
+            'class': 'clients.line.LineProtocolFactory'
+            'port': 90
+        },
+        'http': {
+            'class': 'clients.line.HttpProtocolFactory'
+            'port': 99
+        },
     },
-    {
-        'appname': "App2",
-        'bundle_id': "App2 BundleID",
-        'certificate_file':    "path_to_certificate_file_2",
-        'privatekey_file':     "path_to_privatekey_file_2"
-    },
+
+    'apps': {
+        'app1': {
+            'bundle_id': "App1 BundleID",
+            'certificate_file':    "path_to_certificate_file_1.pem",
+            'privatekey_file':     "path_to_privatekey_file_1.pem"
+        },
+        'app2': {
+            'bundle_id': "App2 BundleID",
+            'certificate_file':    "path_to_certificate_file_2.pem",
+            'privatekey_file':     "path_to_privatekey_file_2.pem"
+        },
+    }
 }
 
